480d7ad5b8287bfe8d5b386887eca7776b03c703
chandupriya <chandupriya@chandupriyas-MacBook-Pro.local>
2018-03-16 21:51:13 +0530

    Istudents deployment fixies

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/content}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, '2.2.2' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, '2.2.5' # Edit this if you are using MRI Ruby
 #set :stage, :production
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
@@ -79,8 +79,8 @@ namespace :deploy do
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
-  after  :finishing
-
+  after  :finishing,    :restart
+end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
